252e2f2823cdb6635febfd3feab987098df25d87
jake <jakelwatson86@gmail.com>
2017-01-22 13:18:45 +0000

    Got paginated page view working but not section.

--- quiz_site2/views.py
+++ quiz_site2/views.py
@@ -1,11 +1,28 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
-from django.http import Http404
-from .models import Section
+from .models import Page, Section
 
 
 def index(request):
+    """
+    Adapted from: https://docs.djangoproject.com/en/1.10/topics/pagination/
+    :param request:
+    :return: render(request, template, dictionary of page objects)
+    """
+    pages = Page.objects.all()
+    paginator = Paginator(pages, 1)
+    page = request.GET.get('page')
     try:
-        sections = Section.objects.all()
-    except Section.DoesNotExist:
-        raise Http404("Section does not exist")
-    return render(request, 'section_test.html', {'section': sections})
+        pages = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        pages = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        pages = paginator.page(paginator.num_pages)
+    return render(request, 'page.html', {'pages': pages})
+
+
+def section(request):
+    return render(request, 'section.html', {'sections': Section.objects.all()})
+
